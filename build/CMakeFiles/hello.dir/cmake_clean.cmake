536631bdff87e5ec9f7c12895f83575b712e9fd5
cynepton <aderonmuisaac@gmail.com>
2020-04-28 17:31:35 +0100

    Added Gazebo files
    Initial upload Gazebo files for the projects

---
+++ build/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.dir/script/welcome_message.cpp.o"
+  "libhello.pdb"
+  "libhello.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
